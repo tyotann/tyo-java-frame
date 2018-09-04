e576fb8b1e90d7890941ac3b3f612f678656aa69
zhaoli <zl888888>
2018-09-04 17:44:01 +0800

    OpenAPIJsonServlet新增限流功能

--- src/com/ihidea/component/api/json/OpenAPIJsonServlet.java
+++ src/com/ihidea/component/api/json/OpenAPIJsonServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.apache.commons.lang.ClassUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
@@ -42,78 +43,88 @@ import com.ihidea.core.util.SystemUtilsEx;
 
 /**
  * 手机服务器端请求
- * 
+ *
  * @author TYOTANN
  */
 public class OpenAPIJsonServlet extends HttpServlet {
-    
+
     private static final long serialVersionUID = -7813617057723596671L;
-    
+
     private static Logger logger = LoggerFactory.getLogger(OpenAPIJsonServlet.class);
-    
+
     private Map<String, Method> mobileMethodMap = new HashMap<String, Method>();
-    
+
     private Map<String, OpenAPIMethod> mobileMethodAnnoMap = new HashMap<String, OpenAPIMethod>();
-    
+
     private Map<String, IAPISign> signclassMap = new HashMap<String, IAPISign>();
-    
+
     private String servletName;
-    
+
     private String ipRanges;
-    
+
+    private double rateLimit;
+
+    private RateLimiter rateLimiter;
+
+
+
     /**
      * 初始化，扫描系统中所有的mobileMethod
      */
     @Override
     public void init() throws ServletException {
-        
+
         try {
             Enumeration<String> paramEnum = getInitParameterNames();
-            
+
             if (paramEnum != null) {
                 while (paramEnum.hasMoreElements()) {
                     String paramName = paramEnum.nextElement();
-                    
+
                     if ("signclass".equals(paramName)) {
-                        signclassMap.put("default", (IAPISign)ClassUtils.getClass(getInitParameter(paramName)).newInstance());
+                        signclassMap.put("default", (IAPISign) ClassUtils.getClass(getInitParameter(paramName)).newInstance());
                     } else if (StringUtils.startsWith(paramName, "signclass-")) {
                         signclassMap.put(StringUtils.substring(paramName, "signclass-".length()),
-                            (IAPISign)ClassUtils.getClass(getInitParameter(paramName)).newInstance());
+                                (IAPISign) ClassUtils.getClass(getInitParameter(paramName)).newInstance());
                     }
                 }
             }
-            
+
             this.servletName = StringUtils.defaultIfEmpty(getInitParameter("servletName"), StringUtils.EMPTY);
             this.ipRanges = getInitParameter("ipRanges");
-            
+            this.rateLimit = Double.parseDouble(StringUtils.defaultIfEmpty(getInitParameter("rateLimit"), "0"));
+            if(this.rateLimit > 0.0) {
+                this.rateLimiter = RateLimiter.create(rateLimit);
+            }
+
             Map<String, Object> openClz = SpringContextLoader.getBeansWithAnnotation(OpenAPI.class);
-            
+
             if (openClz != null) {
                 for (Object clzObj : openClz.values()) {
-                    
+
                     List<Method> methodList = ClassUtilsEx.getClassMethodByAnnotation(clzObj.getClass(), OpenAPIMethod.class);
-                    
+
                     for (Method method : methodList) {
-                        
+
                         OpenAPIMethod methodAnno = method.getAnnotation(OpenAPIMethod.class);
-                        
+
                         String methodName = methodAnno.methodName();
-                        
+
                         if (StringUtils.isBlank(methodName)) {
                             methodName = method.getDeclaringClass().getSimpleName() + "." + method.getName();
                         }
-                        
+
                         // 接口支持版本号
                         if (StringUtils.isNotBlank(methodAnno.version())) {
                             methodName = methodName + "#" + methodAnno.version();
                         }
-                        
+
                         if (servletName.equals(methodAnno.servletName())) {
-                            
+
                             if (mobileMethodMap.containsKey(methodName) || mobileMethodAnnoMap.containsKey(methodName)) {
                                 throw new ServiceException("API接口:" + methodName + "有重复定义,请检查代码!");
                             }
-                            
+
                             mobileMethodMap.put(methodName, method);
                             mobileMethodAnnoMap.put(methodName, methodAnno);
                         }
@@ -125,59 +136,64 @@ public class OpenAPIJsonServlet extends HttpServlet {
             throw new ServletException(e.getMessage(), e);
         }
     }
-    
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+
         MJSONResultEntity result = new MJSONResultEntity();
-        
+
         // 得到参数
         Map<String, Object> paramMap = null;
-        
+
         try {
-            
+
+            // 限流，如果被限流客户端返回code=429
+            if(rateLimit > 0.0 && !rateLimiter.tryAcquire()) {
+                throw new ServiceException("访问频率超过限制");
+            }
+
             // 访问IP检查
             if (StringUtils.isNotBlank(ipRanges) && !CommonUtilsEx.isIPAddressInRange(SystemUtilsEx.getClientIpSingle(request), ipRanges)) {
                 throw new ServiceException("IP地址不允许访问");
             }
-            
+
             // 得到参数
             paramMap = getParam(request, response);
-            
+
             if (StringUtils.isBlank(request.getPathInfo())) {
                 throw new ServiceException("请求的接口格式错误");
             }
-            
+
             // 函数名
             String methodName = request.getPathInfo().substring(1);
-            
+
             // 这里塞的时候不带版本号
             paramMap.put("FRAMEmethodName", methodName);
-            
+
             // 调用函数版本号:如果不在header中,则从param中获得
             String methodVersion = request.getHeader("X-SC-version");
-            methodVersion = StringUtils.isNotBlank(methodVersion) ? methodVersion : (String)paramMap.get("X-SC-version");
-            
+            methodVersion = StringUtils.isNotBlank(methodVersion) ? methodVersion : (String) paramMap.get("X-SC-version");
+
             // 函数名带版本号信息,用于反射
             String methodNameWithVersion = StringUtils.isBlank(methodVersion) ? methodName : methodName + "#" + methodVersion;
-            
+
             OpenAPIMethod mobileMethod = mobileMethodAnnoMap.get(methodNameWithVersion);
-            
+
             // 基本检查
             {
                 if (mobileMethod == null) {
                     throw new ServiceException("请求的接口:" + methodNameWithVersion + "在服务器端没有定义,请检查!");
                 }
             }
-            
+
             // 校验检查
             if (mobileMethod.verify() != OpenAPINoneVerify.class) {
-                
+
                 IAPIVerify verifyMethod = SpringContextLoader.getBean(mobileMethod.verify());
-                
+
                 if (verifyMethod != null) {
                     boolean isVerify = verifyMethod.verify(paramMap);
-                    
+
                     if (!isVerify) {
                         throw new ServiceException(MJSONResultEntity.RESULT_SESSION_ERROR, "登录失败,请登录!");
                     }
@@ -185,31 +201,31 @@ public class OpenAPIJsonServlet extends HttpServlet {
                     throw new ServiceException("未找到定义的类:" + mobileMethod.verify());
                 }
             }
-            
+
             // 请求方法
             result.setData(requestMethod(methodNameWithVersion, mobileMethod, paramMap));
-            
+
         } catch (Exception e) {
-            
+
             Throwable rootThrowable = ExceptionUtils.getRootCause(e) == null ? e : ExceptionUtils.getRootCause(e);
-            
+
             String errorText = rootThrowable.getMessage() == null ? String.valueOf(rootThrowable) : rootThrowable.getMessage();
-            
+
             if (rootThrowable instanceof ServiceException) {
-                
-                ServiceException se = ((ServiceException)rootThrowable);
-                
+
+                ServiceException se = ((ServiceException) rootThrowable);
+
                 // 如果存在code,但是无text,且定义了message_i18n,则从message中取得具体的text值
                 errorText = se.getMessage();
-                
+
                 logger.debug(errorText);
-                
+
                 if (StringUtils.isNotBlank(se.getCode())) {
                     result.setCode(se.getCode());
                 } else {
                     result.setCode(MJSONResultEntity.RESULT_LOGIC_ERROR);
                 }
-                
+
                 if (se.getData() != null) {
                     result.setData(se.getData());
                 }
@@ -220,74 +236,74 @@ public class OpenAPIJsonServlet extends HttpServlet {
                 logger.error(rootThrowable.getMessage(), rootThrowable);
                 result.setCode(MJSONResultEntity.RESULT_EXCEPTION);
             }
-            
+
             result.setText(errorText);
         } finally {
             ServletUtilsEx.renderJson(response, result);
         }
     }
-    
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
-    
+
     @Override
     public void destroy() {
         mobileMethodMap.clear();
         mobileMethodAnnoMap.clear();
     }
-    
+
     /**
      * 得到request中的参数
-     * 
+     *
      * @param request
      * @return
      * @throws IOException
      */
     @SuppressWarnings("unchecked")
     private Map<String, Object> getParam(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        
+
         // web中接口请求，用jquery的ajax post参数后发现如果参数中有多个且有数组，则参数名是xxx[1]这样的格式
         String paramJsonStr = request.getParameter("FRAMEparams");
-        
+
         // 得到参数
         Map<String, Object> paramMap = null;
-        
+
         if (StringUtils.isNotBlank(paramJsonStr)) {
             paramMap = JSONUtilsEx.deserialize(paramJsonStr, Map.class);
         } else {
-            
+
             paramMap = new HashMap<String, Object>();
-            
-            for (String paramName : (Set<String>)request.getParameterMap().keySet()) {
-                
+
+            for (String paramName : (Set<String>) request.getParameterMap().keySet()) {
+
                 if (StringUtils.isNotBlank(paramName)) {
                     paramMap.put(paramName, request.getParameter(paramName));
                 }
             }
         }
-        
+
         String ipAddress = SystemUtilsEx.getClientIpSingle(request);
         String userAgent = request.getHeader("user-agent");
-        
+
         // header中加入：X-Ca-Timestamp，如果时间戳校验失败，返回当前时间
         // header中加入：X-Ca-Signature，签名 md5(timestamp,data,固定值)
         // 框架读取配置文件，如果设置了某个属性，则说明必须强制使用上面签名
         if (signclassMap.size() > 0) {
-            
+
             Long nowTime = new Date().getTime();
-            
+
             String timestamp = request.getHeader("X-Ca-Timestamp");
-            
+
             String sign = request.getHeader("X-Ca-Signature");
-            
+
             String appVersion = request.getHeader("appVersion");
-            
+
             if (StringUtils.isBlank(timestamp) || StringUtils.isBlank(sign)) {
                 throw new ServiceException(MJSONResultEntity.REQUEST_SIGN_ERROR, "缺少签名信息");
             }
-            
+
             try {
                 if (Math.abs(nowTime - Long.valueOf(timestamp)) > 900000) {
                     throw new ServiceException(MJSONResultEntity.REQUEST_SIGN_TIME_ERROR, String.valueOf(nowTime));
@@ -296,57 +312,57 @@ public class OpenAPIJsonServlet extends HttpServlet {
                 logger.error("[安全检查]-签名时间错误,当前时间:{},签名时间:{},IP地址:{},userAgent:{}", new Object[]{nowTime, timestamp, ipAddress, userAgent});
                 throw new ServiceException(MJSONResultEntity.REQUEST_SIGN_TIME_ERROR, "签名时间错误");
             }
-            
+
             // 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
             String paramStr = SignatureUtils.createLinkString(paramMap);
-            
+
             IAPISign signMethod = !signclassMap.containsKey(appVersion) ? signclassMap.get("default") : signclassMap.get(appVersion);
-            
+
             // 签名验证
             if (!signMethod.sign(appVersion, paramStr, timestamp, sign)) {
                 logger.error("[安全检查]-签名错误,签名类:{},客户端签名:{},IP地址:{},userAgent:{},版本号:{}",
-                    new Object[]{signMethod.getClass().getSimpleName(), sign, ipAddress, userAgent, appVersion});
+                        new Object[]{signMethod.getClass().getSimpleName(), sign, ipAddress, userAgent, appVersion});
                 throw new ServiceException(MJSONResultEntity.REQUEST_SIGN_ERROR, "签名错误");
             }
         }
-        
+
         // 分页处理
         if (paramMap.containsKey("page")) {
-            
+
             if (!paramMap.containsKey("pagecount")) {
                 throw new ServiceException("参数列表中存在参数page时,请同时传入分页参数:pagecount!");
             }
-            
+
             // 手机端如果需要总页数,传入totalCount=null，如果不传则代表不需要总页数信息）(设置为0，这样不会再算一遍count)
             Integer totalCount = null;
             if (paramMap.get("totalcount") != null) {
                 totalCount = Integer.valueOf(String.valueOf(paramMap.get("totalcount")));
             }
-            
+
             PageLimitHolderFilter.setContext(Integer.valueOf(String.valueOf(paramMap.get("page"))),
-                Integer.valueOf(String.valueOf(paramMap.get("pagecount"))), totalCount);
+                    Integer.valueOf(String.valueOf(paramMap.get("pagecount"))), totalCount);
             PageLimitHolderFilter.getContext().setLimited(true);
         }
-        
+
         String appid = StringUtils.isNotBlank(CoreConstants.getProperty("application.appid"))
-            ? CoreConstants.getProperty("application.appid") : (String)paramMap.get("appid");
-        
+                ? CoreConstants.getProperty("application.appid") : (String) paramMap.get("appid");
+
         // 建立mobileInfo对象
-        MobileInfo mobileInfo = new MobileInfo((String)paramMap.get("userId"), (String)paramMap.get("deviceid"), appid);
+        MobileInfo mobileInfo = new MobileInfo((String) paramMap.get("userId"), (String) paramMap.get("deviceid"), appid);
         paramMap.put("mobileInfo", mobileInfo);
-        
+
         // 塞入线程变量
         LocalAttributeHolder.getContext().put("appid", appid);
         LocalAttributeHolder.getContext().put("userid", paramMap.get("userId"));
         LocalAttributeHolder.getContext().put("mobileInfo", mobileInfo);
         LocalAttributeHolder.getContext().put("ipAddress", ipAddress);
-        
+
         if (!StringUtils.isBlank(userAgent)) {
-            
+
             userAgent = userAgent.toLowerCase();
-            
+
             String platform = "mobile";
-            
+
             if (userAgent.indexOf("iphone") > -1) {
                 platform = "iphone";
             } else if (userAgent.indexOf("ipad") > -1) {
@@ -356,20 +372,20 @@ public class OpenAPIJsonServlet extends HttpServlet {
             }
             LocalAttributeHolder.getContext().put("platform", platform);
         }
-        
+
         // 如果传入参数是String类型，则做xss过滤
         for (String key : paramMap.keySet()) {
             if (paramMap.get(key) != null && paramMap.get(key) instanceof String) {
-                paramMap.put(key, StringUtilsEx.escapeXss((String)paramMap.get(key)));
+                paramMap.put(key, StringUtilsEx.escapeXss((String) paramMap.get(key)));
             }
         }
-        
+
         return paramMap;
     }
-    
+
     /**
      * 请求服务内容并反射后到protobuf
-     * 
+     *
      * @param methodName
      * @param openMethod
      * @param paramMap
@@ -377,16 +393,16 @@ public class OpenAPIJsonServlet extends HttpServlet {
      * @throws Exception
      */
     private Object requestMethod(String methodName, OpenAPIMethod openMethod, Map<String, Object> paramMap) throws Exception {
-        
+
         // 请求服务
         Method method = mobileMethodMap.get(methodName);
-        
+
         // 开始支持分页，之前的信息查詢不需要分頁
         if (PageLimitHolderFilter.getContext() != null) {
             PageLimitHolderFilter.getContext().setLimited(false);
         }
-        
+
         return ClassUtilsEx.invokeMethod(method.getDeclaringClass().getSimpleName(), method.getName(), paramMap);
     }
-    
+
 }
