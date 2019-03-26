dbcc252136d0afabc6bef52428c21dc0c989fe05
zhaoli <li.zhao@wanbangauto.com>
2019-03-26 10:24:19 +0800

    新增动态事务管理器，只要存在事务就强制使用写库作为数据源,加上日志

--- src/com/ihidea/core/support/dataSource/DynamicDataSourceTransactionManager.java
+++ src/com/ihidea/core/support/dataSource/DynamicDataSourceTransactionManager.java
@@ -1,10 +1,13 @@
 package com.ihidea.core.support.dataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 
 public class DynamicDataSourceTransactionManager extends DataSourceTransactionManager {
 
+    private final static Logger logger = LoggerFactory.getLogger(DynamicDataSourceInterceptor.class);
     /**
      * 本方法是如果开启了事务,才执行.
      * @param transaction
@@ -12,6 +15,7 @@ public class DynamicDataSourceTransactionManager extends DataSourceTransactionMa
      */
     @Override
     protected void doBegin(Object transaction, TransactionDefinition definition) {
+        logger.debug("开启事务, 设置数据源为:" + DynamicDataSourceEnum.WRITE.name());
         // 只要开启了事务, 就使用写库作为数据源
         DynamicDataSourceManager.setRoute(DynamicDataSourceEnum.WRITE.name().toLowerCase());
         super.doBegin(transaction, definition);
